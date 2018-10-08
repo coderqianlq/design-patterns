743ca8000ffa99f58d67e1f90d976a6af994531d
qianlq <1242202279@qq.com>
2018-10-08 20:30:15 +0800

    feat(过滤器模式):添加过滤器实例代码

---
+++ src/cn/qianlq/filter/object/Criteria.java
@@ -0,0 +1,14 @@
+package cn.qianlq.filter.object;
+
+import java.util.List;
+
+/**
+ * @author qianliqing
+ * @date 2018-10-08 下午7:35
+ * mail: qianlq0824@gmail.com
+ */
+
+public interface Criteria {
+
+    List<Person> meetCriteria(List<Person> persons);
+}
---
+++ src/cn/qianlq/filter/object/CriteriaFemale.java
@@ -0,0 +1,24 @@
+package cn.qianlq.filter.object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author qianliqing
+ * @date 2018-10-08 下午7:43
+ * mail: qianlq0824@gmail.com
+ */
+
+public class CriteriaFemale implements Criteria {
+
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        List<Person> femalePersons = new ArrayList<>();
+        for (Person person : persons) {
+            if (person.getGender().equalsIgnoreCase("FEMALE")) {
+                femalePersons.add(person);
+            }
+        }
+        return femalePersons;
+    }
+}
