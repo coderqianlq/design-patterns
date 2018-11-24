ee60ad1b3ed9a1af63acaf836dc7f5b0cc617e8a
qianlq <1242202279@qq.com>
2018-11-24 23:49:08 +0800

    feat(抽象工厂模式): 更新抽象工厂模式代码

--- src/cn/qianlq/abstractfactory/AbstractFactoryPatternDemo.java
+++ src/cn/qianlq/abstractfactory/AbstractFactoryPatternDemo.java
@@ -1,6 +1,7 @@
 package cn.qianlq.abstractfactory;
 
-import cn.qianlq.abstractfactory.factory.AbstractFactory;
+import cn.qianlq.abstractfactory.factory.BlueCircleFactory;
+import cn.qianlq.abstractfactory.factory.RedRectangleFactory;
 import cn.qianlq.abstractfactory.object.color.Color;
 import cn.qianlq.abstractfactory.object.shape.Shape;
 
@@ -11,47 +12,22 @@ import cn.qianlq.abstractfactory.object.shape.Shape;
  */
 
 public class AbstractFactoryPatternDemo {
+
     public static void main(String[] args) {
-        //获取形状工厂
-        AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE");
+        BlueCircleFactory bcf = new BlueCircleFactory();
 
-        //获取形状为 Circle 的对象
-        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        Shape circle = bcf.getShape();
+        circle.draw();
 
-        //调用 Circle 的 draw 方法
-        shape1.draw();
+        Color blue = bcf.getColor();
+        blue.fill();
 
-        //获取形状为 Rectangle 的对象
-        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+        RedRectangleFactory rrf = new RedRectangleFactory();
 
-        //调用 Rectangle 的 draw 方法
-        shape2.draw();
+        Shape rectangle = rrf.getShape();
+        rectangle.draw();
 
-        //获取形状为 Square 的对象
-        Shape shape3 = shapeFactory.getShape("SQUARE");
-
-        //调用 Square 的 draw 方法
-        shape3.draw();
-
-        //获取颜色工厂
-        AbstractFactory colorFactory = FactoryProducer.getFactory("COLOR");
-
-        //获取颜色为 Red 的对象
-        Color color1 = colorFactory.getColor("RED");
-
-        //调用 Red 的 fill 方法
-        color1.fill();
-
-        //获取颜色为 Green 的对象
-        Color color2 = colorFactory.getColor("Green");
-
-        //调用 Green 的 fill 方法
-        color2.fill();
-
-        //获取颜色为 Blue 的对象
-        Color color3 = colorFactory.getColor("BLUE");
-
-        //调用 Blue 的 fill 方法
-        color3.fill();
+        Color red = rrf.getColor();
+        red.fill();
     }
 }
