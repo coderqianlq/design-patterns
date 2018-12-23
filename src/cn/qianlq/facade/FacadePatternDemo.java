79702d61f5e0ca1986a77b69e3644d13be1b5481
qianlq <1242202279@qq.com>
2018-12-23 11:28:02 +0800

    :fallen_leaf: feat(外观模式): 更新外观模式示例代码

--- src/cn/qianlq/facade/FacadePatternDemo.java
+++ src/cn/qianlq/facade/FacadePatternDemo.java
@@ -13,6 +13,8 @@ public class FacadePatternDemo {
     public static void main(String[] args) {
         ShapeMaker shapeMaker = new ShapeMaker();
 
+        shapeMaker.drawAll();
+
         shapeMaker.drawCircle();
         shapeMaker.drawRectangle();
         shapeMaker.drawSquare();
